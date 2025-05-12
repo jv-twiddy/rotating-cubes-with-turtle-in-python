ba877a6f3ace2a2270d908cae01577b8d3a61b26
jv-twiddy <jvt202@exeter.ac.uk>
2025-05-12 22:07:59 +0100

    triangle based pyramid

--- gen_demo.py
+++ gen_demo.py
@@ -10,46 +10,57 @@ screen.setup(1.0, 1.0)
 # Begin!
 t = turtle.Turtle()
 t.speed(0)
-pos_x = 0
-pos_y = 0 
-pos_z = 0
 fe = 1 
 # cube 
 cube = {
     "coords3D" : [
-  [pos_x-fe,pos_y-fe,pos_z-fe],
-  [pos_x+fe,pos_y-fe,pos_z-fe],
-  [pos_x+fe,pos_y+fe,pos_z-fe],
-  [pos_x-fe,pos_y+fe,pos_z-fe],
-  [pos_x-fe,pos_y-fe,pos_z+fe],
-  [pos_x+fe,pos_y-fe,pos_z+fe],
-  [pos_x+fe,pos_y+fe,pos_z+fe],
-  [pos_x-fe,pos_y+fe,pos_z+fe]
+  [-fe,-fe,-fe],
+  [+fe,-fe,-fe],
+  [+fe,+fe,-fe],
+  [-fe,+fe,-fe],
+  [-fe,-fe,+fe],
+  [+fe,-fe,+fe],
+  [+fe,+fe,+fe],
+  [-fe,+fe,+fe]
   ],
     "connections":[0,1,2,3,0,3,7,6,5,4,0,1,5,6,2,6,7,4]
 }
 
+tri_pyrmd = {
+  "coords3D":[
+    [0,0,0.8819195201], # tip is that far from the base 
+    [0,2/3,0],
+    [1/2,-1/3,0],
+    [-1/2,-1/3,0]
+  ],
+  "connections":[0,1,2,0,3,1,3,2]
+}
+
 points3D = [
-  [pos_x-fe,pos_y-fe,pos_z-fe],
-  [pos_x+fe,pos_y-fe,pos_z-fe],
-  [pos_x+fe,pos_y+fe,pos_z-fe],
-  [pos_x-fe,pos_y+fe,pos_z-fe],
-  [pos_x-fe,pos_y-fe,pos_z+fe],
-  [pos_x+fe,pos_y-fe,pos_z+fe],
-  [pos_x+fe,pos_y+fe,pos_z+fe],
-  [pos_x-fe,pos_y+fe,pos_z+fe]
+  [-fe,-fe,-fe],
+  [+fe,-fe,-fe],
+  [+fe,+fe,-fe],
+  [-fe,+fe,-fe],
+  [-fe,-fe,+fe],
+  [+fe,-fe,+fe],
+  [+fe,+fe,+fe],
+  [-fe,+fe,+fe]
   ]
 position = [0,0,0]
-rotation = [10, 50, 10]
+rotation = [30, 50, 10]
 connections = [0,1,2,3,0,3,7,6,5,4,0,1,5,6,2,6,7,4]
 scale = 100
 
 axis6s = [position,rotation,scale]
 
 t.up()
-display_frame(t,cube["coords3D"],position, rotation, cube["connections"],scale)
+# draw here:
 
-axis6s[2] -= 50 
-display_frame_s(t,cube,axis6s)
+
+
+# display_frame(t,cube["coords3D"],position, rotation, cube["connections"],scale)
+
+# axis6s[2] -= 50 
+display_frame_s(t,tri_pyrmd,axis6s)
 
 f = input("end>")
